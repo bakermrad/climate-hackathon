76f07b4ab1a618019bae88f2c86b0c61dc000fd1
bakermrad2 <bakermrad0@gmail.com>
2022-06-07 14:08:12 +0300

    react

---
+++ project/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav  >
+            {/* <img src="logo.png" alt=""> */}
+            <li><a href="google.com">baker</a></li>
+            <li><a href="google.com">logan</a></li>
+            <li><a href="google.com">justen</a></li>
+
+
+        </nav>
+    )
+}
