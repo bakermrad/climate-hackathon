d4bbc63c53b50a2e73dc349ac2546ca04d669cb2
bakermrad2 <bmm338@student.bau.edu.lb>
2022-06-07 16:20:03 -0900

    1.00

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Header from "./components/Header"
 
 export default function App(){
     return(
         <div>
             <Navbar />
+            <Header />
         </div>
     )
 }
---
+++ project/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <section>
+            <h1 className="Header--h1">welcome to our project</h1>
+        </section>
+    )
+}
