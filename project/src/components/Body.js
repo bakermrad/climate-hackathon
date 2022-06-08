c06843ee198da3c298a834b9ff835833119f6f4e
bakermrad2 <bmm338@student.bau.edu.lb>
2022-06-07 19:44:32 -0900

    main

--- project/src/components/Body.js
+++ project/src/components/Body.js
@@ -2,22 +2,23 @@ import React from "react"
 
 export default function Body() {
     return (
-        <section className="Main-body">
-            <div className="problem">
+        <section  className="Main-body">
+            <div id="problem" className="problem">
                 <div className="element1">
                     <h1>problem 1</h1>
-                    <img className="main--imgs" src="images/img1.png" alt="" />
-                    <br />
+                    <img className="main--imgs  img1" src="images/img1.png" alt="" />
+                   
                 </div>
                 <div className="element2">
                     <img className="main--imgs" src="images/img2.jpg" alt="" />
                     <h1>problem 2</h1>
-                    <br />
+                    
                 </div>
-                <div className="element3">
-                    <h1>problem 3</h1>
-                    <img className="main--imgs" src="" alt="" />
-                </div>
+
+
+
+            
+                
             </div>
 
 
--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -2,11 +2,11 @@ import React from "react"
 
 export default function Navbar() {
     return (
-        <nav  >
+        <nav   >
             {/* <img src="logo.png" alt=""> */}
-            <li className="nav-li"><a href="google.com">baker</a></li>
-            <li className="nav-li"><a href="google.com">logan</a></li>
-            <li className="nav-li"><a href="google.com">justen</a></li>
+            <li className="nav-li"><a href="#problem">PROBLEM</a></li>
+            <li className="nav-li"><a href="#problem">SOLUTION</a></li>
+            <li className="nav-li"><a href="#problem">PROJECT</a></li>
 
 
         </nav>
