9a951713f915b7b0af764b56b8fa12ad54a373d2
lagoonie <79497735+lagoonie@users.noreply.github.com>
2022-06-08 20:47:55 -0700

    Azure Map

--- project/src/components/Navbar.js
+++ project/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav   >
             {/* <img src="logo.png" alt=""> */}
             
-            <img src="./images/CetusLogo.png" alt="dfgh" className="logo"/>
+            <img src="./images/CetusLogoWhite.png" alt="dfgh" className="logo"/>
             <li className="nav-li"><a href="#problem">PROBLEM</a></li>
             <li className="nav-li"><a href="#solution">SOLUTION</a></li>
             <li className="nav-li"><a href="#project--con">PROJECT</a></li>
